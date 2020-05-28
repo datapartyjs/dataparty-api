b90a247deda3668f936b86ef69292788f37a1e9f
Alan Meekins <alanm@roshub.io>
2020-05-27 18:11:48 -0700

    endpoint info

--- src/sandbox/index.js
+++ src/sandbox/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   Sandbox: require('./sandbox'),
   SandboxError: require('./sandbox-error'),
+  EndpointInfoSandbox: require('./endpoint-info-sandbox'),
   MiddlewareInfoSandbox: require('./middleware-info-sandbox'),
   MiddlewareExecSandbox: require('./middleware-exec-sandbox')
 }
