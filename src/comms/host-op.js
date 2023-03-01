05f998639a1b916c36e36598b8b4244822417726
alanm <alanm@nodetest.lxd>
2023-03-01 20:04:31 +0000

    host protocol scheme

---
+++ src/comms/host-op.js
@@ -0,0 +1,101 @@
+const Hoek = require('@hapi/hoek')
+const debug = require('debug')('roshub.op')
+const moment = require('moment')
+const EventEmitter = require('eventemitter3')
+
+const OP_STATUS_LEVEL = {
+  Status_Undefined: 'Status_Undefined',
+  Status_None: 'Status_None',
+  Status_Error: 'Status_Error',
+  Status_Warning: 'Status_Warning',
+  Status_Info: 'Status_Info',
+  Status_Debug: 'Status_Debug'
+}
+
+
+const OP_STATES = {
+  Pending: 'Pending',
+  Starting: 'Starting',
+  Running: 'Running',
+  Finished_Success: 'Finished_Success',
+  Finished_Fail: 'Finished_Fail',
+  Finished_Permission_Denied: 'Finished_Permission_Denied',
+  Finished_Invalid_Op: 'Finished_Invalid_Op',
+  Finished_Invalid_Id: 'Finished_Invalid_Id',
+  Finished_Buffer_Full: 'Finished_Buffer_Full'
+}
+
+class Op extends EventEmitter {
+  constructor({msg, input}){
+    super()
+    this.msg = msg      //! original encrypted message
+    this.input = input  //! parsed and validated input
+    this.start = Date.now()
+    this.end = undefined
+    this.state = Op.STATES.Pending
+    this.level = Op.STATUS_LEVELS.Status_Info
+  }
+
+  get op(){
+    return Hoek.reach(this.input, 'op')
+  }
+
+  get id(){
+    return Hoek.reach(this.input, 'id')
+  }
+
+
+  static get STATES(){
+    return OP_STATES
+  }
+
+  static get STATUS_LEVELS(){
+    return OP_STATUS_LEVEL
+  }
+
+  setState(state, level, message){
+
+    this.state = state
+
+    const status = {
+      op: 'status',
+      id: this.id,
+      state: this.state,
+      level: level || Op.STATUS_LEVELS.Status_Info,
+      msg: message || undefined,
+      timestamp: Date.now(),
+      stats: {
+        start: this.start,
+        end: this.end,
+        duration_ms: this.end - this.start
+      }
+    }
+
+    if(this.state == Op.STATES.Finished_Success ||
+      this.state == Op.STATES.Finished_Fail ||
+      this.state == Op.STATES.Finished_Invalid_Op ||
+      this.state == Op.STATES.Finished_Invalid_Id ||
+      this.state == Op.STATES.Finished_Permission_Denied ||
+      this.state == Op.STATES.Finished_Buffer_Full)
+    {
+      debug('set end time')
+      this.end = Date.now()
+      this.emit('finished', true)
+    }
+    else{
+      this.emit('finished', false)
+      this.emit('status', status)
+    }
+  }
+
+  setOpStatusLevel(level){
+    this.level = level
+  }
+
+  setStatus(level, message){
+    this.emit('status', )
+  }
+
+}
+
+module.exports = Op
---
+++ src/comms/protocol-scheme.js
@@ -0,0 +1,82 @@
+const Joi = require('@hapi/joi')
+Joi.objectId = require('joi-objectid')(Joi)
+
+const ID_SCHEME = Joi.alternatives().try(Joi.string(), Joi.number())
+
+const OP_HEADER = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid([
+    'auth',
+    'advertise',
+    'subscribe',
+    'unsubscribe',
+    'publish'
+  ]).required()
+})
+
+
+const AUTH_OP = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid('auth').required(),
+  session: Joi.objectId().required()
+})
+
+const ADVERTISE_OP = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid('advertise').required(),
+  type: Joi.string().required(),
+  topic: Joi.string().required(),
+  queue_size: Joi.number(),
+  latch: Joi.boolean()
+})
+
+const UNADVERTISE_OP = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid('unadvertise').required(),
+  topic: Joi.string().required()
+})
+
+
+const SUBSCRIBE_OP = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid('subscribe').required(),
+  type: Joi.string().required(),
+  topic: Joi.string().required(),
+  compression: Joi.string(),
+  queue_length: Joi.number(),
+  throttle_rate: Joi.number()
+})
+
+const UNSUBSCRIBE_OP = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid('unsubscribe').required(),
+  topic: Joi.string().required()
+})
+
+const PUBLISH_OP = Joi.object().keys({
+  id: ID_SCHEME.required(),
+  op: Joi.string().valid('publish').required(),
+  topic: Joi.string().required(),
+  latch: Joi.boolean(),
+  msg: Joi.any()
+})
+
+
+const ANY_OP = Joi.alternatives().try(
+  ADVERTISE_OP,
+  UNADVERTISE_OP,
+  SUBSCRIBE_OP,
+  UNSUBSCRIBE_OP,
+  PUBLISH_OP
+)
+
+module.exports = {
+  'OP_HEADER': OP_HEADER,
+  'AUTH_OP': AUTH_OP,
+  'ADVERTISE_OP': ADVERTISE_OP,
+  'UNADVERTISE_OP': UNADVERTISE_OP,
+  'SUBSCRIBE_OP': SUBSCRIBE_OP,
+  'UNSUBSCRIBE_OP': UNSUBSCRIBE_OP,
+  'PUBLISH_OP': PUBLISH_OP,
+  'ANY_OP': ANY_OP
+}
