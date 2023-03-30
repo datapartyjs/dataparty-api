01a05f26b8f1fcb20f1e74ce65d021510c548b00
7bit <662932+sevenbitbyte@users.noreply.github.com>
2023-03-30 00:28:45 +0000

    Docs updates (#60)
    * tutorial examples and service documentation
    
    * update readme
    
    * more documentation
    
    * fix badges

--- src/venue/schema/invite.js
+++ src/venue/schema/invite.js
@@ -2,9 +2,9 @@
   uuid: String,
   created: Date,
   owner: Admin | User | Self,
-  type: [ 'Admin', 'User' ],
+  type: [ 'Admin', 'User', 'Party' ],  //! type of account that can be created
   claimedBy: [Admin[], User[]],
   anyoneWithLink: Boolean,
   sendToEmail: String,
-  localHostOnly: Boolean //! true = only allow invite claim over localhost
+  localhostOnly: Boolean //! true = only allow invite claim over localhost
 }
