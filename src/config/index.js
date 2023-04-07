882bcfda0c0588b634556a7ebcbf1f8e16ca36cf
sevenbitbyte <code@nullagent.com>
2023-04-07 21:15:11 +0000

    add to build

--- src/config/index.js
+++ src/config/index.js
@@ -8,3 +8,5 @@ exports.NconfConfig = require('./nconf')
 exports.MemoryConfig = require('./memory')
 exports.JsonFileConfig = require('./json-file')
 exports.LocalStorageConfig = require('./local-storage')
+
+exports.SecureConfig = require('./secure-config')
